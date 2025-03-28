7d9f0f0c3933b39eff8f34398c178569ffa9de19
paramin3 <137262746+paramin3@users.noreply.github.com>
2025-03-28 17:11:38 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 
-const distFolder = path.join(process.cwd(), 'dist', 'project');
+const distFolder = path.resolve('dist/project');
 
 app.use(express.static(distFolder));
 
