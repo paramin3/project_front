6a4812087943b1e0e65f48f7872d2c313990a47b
paramin3 <137262746+paramin3@users.noreply.github.com>
2025-03-28 21:55:37 +0700

    Update class.service.ts

--- src/app/class.service.ts
+++ src/app/class.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
+import { environment } from './environment';
 @Injectable({
   providedIn: 'root'
 })
 export class ClassService {
-  private apiUrl = '/api/classes'; // URL ของ API
+  private baseUrl = `${environment.apiBaseUrl}/api/classes'; // URL ของ API
 
   constructor(private http: HttpClient) {}
 
