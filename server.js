ddc5885822b204af8e3f8e39770fc4c48a56e9e3
paramin3 <137262746+paramin3@users.noreply.github.com>
2025-03-28 17:08:28 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
-// ✅ ใช้ path แบบชัดเจนและปลอดภัยสำหรับ Render
+
 const distFolder = path.join(__dirname, 'dist', 'project');
 
 app.use(express.static(distFolder));
@@ -14,5 +14,5 @@ app.get('*', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`✅ Server is running on port ${port}`);
+  console.log(` Server is running on port ${port}`);
 });
