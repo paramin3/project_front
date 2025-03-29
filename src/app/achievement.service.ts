9459a4164a43d18accd6b14638452fee9238ce1b
paramin3 <137262746+paramin3@users.noreply.github.com>
2025-03-29 21:53:37 +0700

    Update achievement.service.ts

--- src/app/achievement.service.ts
+++ src/app/achievement.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from './environment';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
 @Injectable({
   providedIn: 'root',
 })
